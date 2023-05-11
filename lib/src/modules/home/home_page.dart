628168e1f9ed104d56655a236a018a27e8f5e771
Arthur <arthur.m.ramos@hotmail.com>
2023-05-11 18:57:44 -0300

    feat: create base template

--- lib/src/modules/home/home_page.dart
+++ lib/src/modules/home/home_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import '../../core/ui/helpers/loader.dart';
 import '../../core/ui/helpers/messages.dart';
 import '../../core/ui/styles/colors_app.dart';
+import '../template/base_layout.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -13,18 +14,7 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> with Loader, Messagens {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home Page'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () async {
-          showLoader();
-          await await Future.delayed(const Duration(seconds: 2));
-          hideLoader();
-          showError('Error de alguma coisa');
-        },
-      ),
+    return BaseLayout(
       body: Container(
         child: Container(
           child: Column(
