01431af13ae301b73043876f0f44bf1087828e5a
Arthur <arthur.m.ramos@hotmail.com>
2023-05-24 21:16:05 -0300

    feat: implements product page and item

--- lib/src/modules/products/home/products_page.dart
+++ lib/src/modules/products/home/products_page.dart
@@ -1,10 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:mobx/mobx.dart';
 
+import '../../../core/ui/helpers/loader.dart';
+import '../../../core/ui/helpers/messages.dart';
 import '../../../core/ui/widgets/base_header.dart';
+import 'products_controller.dart';
 import 'widgets/product_item.dart';
 
-class ProductsPage extends StatelessWidget {
+class ProductsPage extends StatefulWidget {
   const ProductsPage({super.key});
+
+  @override
+  State<ProductsPage> createState() => _ProductsPageState();
+}
+
+class _ProductsPageState extends State<ProductsPage> with Loader, Messagens {
+  final controller = Modular.get<ProductsController>();
+  late final ReactionDisposer statusDisposer;
+
+  @override
+  void initState() {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      statusDisposer = reaction((_) => controller.status, (status) {
+        switch (status) {
+          case ProductStateStatus.initial:
+            break;
+          case ProductStateStatus.loading:
+            showLoader();
+            break;
+          case ProductStateStatus.loaded:
+            hideLoader();
+            break;
+          case ProductStateStatus.error:
+            hideLoader();
+            showError('Erro ao buscar produtos');
+            break;
+        }
+      });
+      controller.loadProducts();
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    statusDisposer();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,16 +66,20 @@ class ProductsPage extends StatelessWidget {
             height: 50,
           ),
           Expanded(
-            child: GridView.builder(
-              itemCount: 10,
-              gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-                mainAxisExtent: 280,
-                mainAxisSpacing: 20,
-                maxCrossAxisExtent: 280,
-                crossAxisSpacing: 10,
-              ),
-              itemBuilder: (context, index) {
-                return ProductItem();
+            child: Observer(
+              builder: (_) {
+                return GridView.builder(
+                  itemCount: controller.products.length,
+                  gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+                    mainAxisExtent: 280,
+                    mainAxisSpacing: 20,
+                    maxCrossAxisExtent: 280,
+                    crossAxisSpacing: 10,
+                  ),
+                  itemBuilder: (context, index) {
+                    return ProductItem(product: controller.products[index]);
+                  },
+                );
               },
             ),
           )
